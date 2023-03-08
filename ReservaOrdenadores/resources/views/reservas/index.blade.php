86c853f0a30df680dc20478eb1116ba429142313
Reyvaj14 <javier.rodriguezhoyo@iesmiguelherrero.com>
2023-03-08 15:44:50 +0100

    #7 cambio del boton en el index

--- ReservaOrdenadores/resources/views/reservas/index.blade.php
+++ ReservaOrdenadores/resources/views/reservas/index.blade.php
@@ -5,6 +5,7 @@
         <div class="border-blue-600 border-2 flex mb-4">
             <div class="w-1/3 p-2">
                 <input type="date" id="calendario" name="calendario">
+                <a href="{{route('reservas.create')}}" class="bazul">Reservar</a>
             </div>
         </div>
 @endsection
