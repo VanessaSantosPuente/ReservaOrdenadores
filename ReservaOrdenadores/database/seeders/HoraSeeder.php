18fc2019028714ec4c1a6db7d60d0fd26bc52aa6
Reyvaj14 <javier.rodriguezhoyo@iesmiguelherrero.com>
2023-03-08 15:29:04 +0100

    #6 cambio en los seederes simula curso academico

--- ReservaOrdenadores/database/seeders/HoraSeeder.php
+++ ReservaOrdenadores/database/seeders/HoraSeeder.php
@@ -14,47 +14,51 @@ class HoraSeeder extends Seeder
     public function run(): void
     {
         $hora = new Hora();
-        $hora->intervalo = '8:00-9:00';
+        $hora->intervalo = '8:30-9:20';
         $hora->save();
 
         $hora1 = new Hora();
-        $hora1->intervalo = '9:00-10:00';
+        $hora1->intervalo = '9:20-10:10';
         $hora1->save();
 
         $hora2 = new Hora();
-        $hora2->intervalo = '10:00-11:00';
+        $hora2->intervalo = '10:25-11:15';
         $hora2->save();
 
         $hora3 = new Hora();
-        $hora3->intervalo = '11:00-12:00';
+        $hora3->intervalo = '11:15-12:05';
         $hora3->save();
 
         $hora4 = new Hora();
-        $hora4->intervalo = '12:00-13:00';
+        $hora4->intervalo = '12:20-13:10';
         $hora4->save();
 
         $hora5 = new Hora();
-        $hora5->intervalo = '13:00-14:00';
+        $hora5->intervalo = '13:10-14:00';
         $hora5->save();
 
         $hora6 = new Hora();
-        $hora6->intervalo = '15:00-16:00';
+        $hora6->intervalo = '14:45-15:35';
         $hora6->save();
 
         $hora7 = new Hora();
-        $hora7->intervalo = '16:00-17:00';
+        $hora7->intervalo = '15:35-16:25';
         $hora7->save();
 
         $hora8 = new Hora();
-        $hora8->intervalo = '17:00-18:00';
+        $hora8->intervalo = '16:40-17:30';
         $hora8->save();
 
         $hora9 = new Hora();
-        $hora9->intervalo = '18:00-19:00';
+        $hora9->intervalo = '17:30-18:20';
         $hora9->save();
 
         $hora10 = new Hora();
-        $hora10->intervalo = '19:00-20:00';
+        $hora10->intervalo = '18:35-19:25';
         $hora10->save();
+
+        $hora11 = new Hora();
+        $hora11->intervalo = '19:25-20:15';
+        $hora11->save();
     }
 }
--- ReservaOrdenadores/database/seeders/JornadaSeeder.php
+++ ReservaOrdenadores/database/seeders/JornadaSeeder.php
@@ -14,6 +14,7 @@ class JornadaSeeder extends Seeder
      */
     public function run(): void
     {
+        /*
         $jornada = new Jornada();
         $jornada->dia = '09';
         $jornada->mes = '03';
@@ -34,5 +35,33 @@ class JornadaSeeder extends Seeder
         $jornada2->anio = '2023';
         $jornada2->idHora = '6';
         $jornada2->save();
+        */
+        
+        //
+        $fecha_inicio = "2022-09-09";
+        $fecha_fin = "2023-06-26";
+        // Convertir las fechas a timestamp
+        $timestamp_inicio = strtotime($fecha_inicio);
+        $timestamp_fin = strtotime($fecha_fin);
+        
+        // Recorrer las fechas y mostrarlas 1 dia = 86400 segundos
+        for ($i = $timestamp_inicio; $i <= $timestamp_fin; $i += 86400) {
+            //sacar el dia en formaato dia de la semana de 0-6
+            $numero_dia = date("w", $i);
+  
+            // Saltar la iteración si el día es sábado (6) o domingo (0)
+            if ($numero_dia != 6 && $numero_dia != 0) {
+                for($x = 1; $x <=12; $x++){
+                    $jornada = new Jornada();
+                    $jornada->dia = date('d',$i);
+                    $jornada->mes = date('m',$i);
+                    $jornada->anio = date('Y',$i);
+                    $jornada->idHora = $x;
+                    $jornada->save();
+                }
+            }   
+            
+            
+        }
     }
 }
