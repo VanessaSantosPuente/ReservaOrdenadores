a37cce5192ac9cb28afcfce88ce5af21b0c813b6
VanessaSP <53338433+VanxyS@users.noreply.github.com>
2023-03-08 20:32:22 +0100

    #8 Primera versión de la interfaz gráfica

--- ReservaOrdenadores/resources/views/reservas/create.blade.php
+++ ReservaOrdenadores/resources/views/reservas/create.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.plantilla')
 @section('titulo', 'Ordenadores Disponibles')
 @section('contenido')
-    <h1 class="text-3xl font-bold underline">Ordenadores Disponibles</h1>
-        <div class="border-blue-600 border-2 flex mb-4">
-            <div class="w-1/3 p-2">
-
-            </div>
-        </div>
+<h1 class="text-3xl font-bold underline">Ordenadores Disponibles</h1>
+<div class="border-blue-600 border-2 flex mb-4">
+    <div class="w-1/3 p-2">
+            @foreach ($ordenadores as $ordenador)
+                <div class="p-6 shadow-2xl">$ordenador['name']</div>
+            @endforeach
+    </div>
+</div>
 @endsection
